ec27b800223415515ca9c974c5cf970d827e4681
daniel301r <daniel.munns247@gmail.com>
2019-10-12 13:27:08 +0100

    filter list styling

--- src/components/pet page/Filters.js
+++ src/components/pet page/Filters.js
@@ -2,34 +2,76 @@ import React, { Component } from 'react';
 
 class Filters extends Component {
 
+    state = {
+        isExpanded: false
+    }
+
+    toggleOptions = () => {
+        this.setState({ isExpanded: !this.state.isExpanded })
+    }
+
     render() {
         return (
-            // think about how to organise this as a form or not
-            <div className="filters">
-                <div>
+            // should really make these into small stateless components and pass in data
+            <div className="filters-container">
+                <div className="filter-container">
                     <div>Breed</div>
                     <button>Any<i className="fas fa-chevron-down"></i></button>
                 </div>
-                {/* <div>
-                    <label htmlFor="age">Age</label>
-                    <input type="text" id="age"/>
+                <div className="filter-container">
+                    <div>Age</div>
+                    <button onClick={this.toggleOptions}>Any<i className="fas fa-chevron-down"></i></button>
                 </div>
-                <div>
-                    <label htmlFor="size">Size</label>
-                    <input type="text" id="size"/>
+                <ul className={this.state.isExpanded ? "filter-options-expanded" : "filter-options"}>
+                    <li>
+                        Baby
+                        <div>
+                            <span>(0)</span>
+                            <i className="fas fa-plus-circle"></i>
+                        </div>
+                    </li>
+                    <li>
+                        Young
+                        <div>
+                            <span>(0)</span>
+                            <i className="fas fa-plus-circle"></i>
+                        </div>
+                    </li>
+                    <li>
+                        Adult
+                        <div>
+                            <span>(0)</span>
+                            <i className="fas fa-plus-circle"></i>
+                        </div>
+                    </li>
+                    <li>
+                        Senior
+                        <div>
+                            <span>(0)</span>
+                            <i className="fas fa-plus-circle"></i>
+                        </div>
+                    </li>
+                </ul>
+                <div className="filter-container">
+                    <div>Size</div>
+                    <button>Any<i className="fas fa-chevron-down"></i></button>
                 </div>
-                <div>
-                    <label htmlFor="gender">Gender</label>
-                    <input type="text" id="gender"/>
+                <div className="filter-container">
+                    <div>Gender</div>
+                    <button>Any<i className="fas fa-chevron-down"></i></button>
                 </div>
-                <div>
-                    <label htmlFor="color">Color</label>
-                    <input type="text" id="color"/>
+                <div className="filter-container">
+                    <div>Good With</div>
+                    <button>Any<i className="fas fa-chevron-down"></i></button>
                 </div>
-                <div>
-                    <label htmlFor="good-with">Good With</label>
-                    <input type="text" id="good-with"/>
-                </div> */}
+                <div className="filter-container">
+                    <div>Coat Length</div>
+                    <button>Any<i className="fas fa-chevron-down"></i></button>
+                </div>
+                <div className="filter-container">
+                    <div>Color</div>
+                    <button>Any<i className="fas fa-chevron-down"></i></button>
+                </div>
             </div>
         );
     }
