8ab042df6064d528983b0bf92edb8cdd7e54a7f0
daniel301r <daniel.munns247@gmail.com>
2019-10-25 11:26:32 +0100

    css updates

--- src/components/petsPage/PetCard.js
+++ src/components/petsPage/PetCard.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const PetCard = ({ name, age, species, img, id }) => {
     return (
-        <div className="pet-card">
-            <Link to={"/singlePet/" + id}>
+        <Link to={"/singlePet/" + id}>
+            <div className="pet-card">
                 {/* need to figure out what to do with the imgs - there are different sizes etc. */}
                 <img src={img} alt=""/>
                 <div className="pet-info">
@@ -13,8 +13,9 @@ const PetCard = ({ name, age, species, img, id }) => {
                     <p>{species}</p>
                     <p>{age}</p>
                 </div>
-            </Link>
-        </div>
+            
+            </div>
+        </Link>
     );
 };
 
